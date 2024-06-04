91e231639fba965de1dad831cf97c6333c895dba
Tushar Chandra <cout.destiny@gmail.com>
2024-06-04 11:49:04 +0530

    Time: 5 ms (29.78%) | Memory: 9.1 MB (26.04%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        int n = s.size();
+
+        if(n==1) return 1;
+
+        bool hasodd = false;
+        unordered_map<char, int> mp;
+        int ans = 0;
+
+        for(int i=0; i<n; i++){
+            mp[s[i]]++;
+        }
+
+        for(auto x :mp){
+            if(x.second>1 && x.second%2!=0){
+                hasodd = true;
+            }
+        }
+
+        for(auto x: mp){
+            if(x.second>1){
+                ans += x.second - x.second%2;
+            }
+        }
+
+        if(ans < n){
+            ans++;
+        }
+
+        // ans++;
+
+        return ans;
+    }
+};

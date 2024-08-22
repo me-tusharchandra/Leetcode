30f2744514bd4e74a4cca5ae8ae7b5bddcdb3ea4
Tushar Chandra <cout.destiny@gmail.com>
2024-08-22 17:00:40 +0530

    Time: 4 ms (33.76%) | Memory: 8.3 MB (30.92%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
-
-    int ncr(int n, int r){
-
-        int res = 1;
-        
-        for(int i=0; i<r; i++){
-            res = res*(n-i);
-            res = res/(i+1);
-        }
-
-        return res;
-    }
-
-    vector<int> rowgen(int n){
+    vector<int> logic(int n){
         vector<int> ans;
 
-        for(int i=0; i<n; i++){
-            ans.push_back(ncr(n-1, i));
+        int res = 1;
+        ans.push_back(res);
+
+        for(int i=1; i<n; i++){
+            res = res*(n-i);
+            res = res/(i);
+            ans.push_back(res);
+            // cout << res;
         }
 
         return ans;
@@ -27,7 +20,7 @@ public:
         vector<vector<int>> ans;
 
         for(int i=1; i<=numRows; i++){
-            ans.push_back(rowgen(i));
+            ans.push_back(logic(i));
         }
 
         return ans;
